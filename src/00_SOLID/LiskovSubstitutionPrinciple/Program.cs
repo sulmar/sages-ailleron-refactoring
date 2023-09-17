d560bee7753bd933d4dc7461a1ab11b2307dcbf0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-17 21:01:44 +0200

    Initial commit

---
+++ src/00_SOLID/LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,38 @@
+﻿// Zasada podstawiania Liskov (Liskov Substitution Principle) - LSP  
+// w miejscu klasy bazowej można użyć dowolnej klasy pochodnej (zgodność wszystkich metod).
+
+// Przykład łamiący zasadę podstawiania Liskov
+
+
+
+public class Car
+{
+    public virtual void StartEngine()
+    {
+        Console.WriteLine("Engine started.");
+    }
+
+    public virtual void Accelerate()
+    {
+        Console.WriteLine("Car is accelerating.");
+    }
+}
+
+public class ElectricCar : Car
+{
+    public override void StartEngine()
+    {
+        Console.WriteLine("Electric motor started.");
+    }
+
+    public override void Accelerate()
+    {
+        Console.WriteLine("Electric car is accelerating.");
+    }
+
+    public void ChargeBattery()
+    {
+        Console.WriteLine("Battery is charging.");
+    }
+}
+
