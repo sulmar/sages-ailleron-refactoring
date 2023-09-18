ffc98f48676ae11fcef0c1fc4ef100f81f317bf9
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-18 14:23:04 +0200

    Zastosowanie prostej fabryki statycznej

--- src/01_CreationalsPatterns/SimpleFactoryPattern/Models/Visit.cs
+++ src/01_CreationalsPatterns/SimpleFactoryPattern/Models/Visit.cs
@@ -2,13 +2,70 @@
 
 namespace SimpleFactoryPattern
 {
-    public class Visit
+
+    public class VisitFactory
+    {
+        public static Visit Create(string symbol, TimeSpan duration, decimal pricePerHour)
+        {
+            return symbol switch // Match Patterns
+            {
+                "N" => new NfzVisit(duration, pricePerHour),
+                "P" => new PrivateVisit(duration, pricePerHour),
+                "F" => new CompanyVisit(duration, pricePerHour),
+                _ => throw new NotSupportedException(nameof(symbol)),
+            };
+        }
+    }
+
+    public class NfzVisit : Visit
+    {
+        public NfzVisit(TimeSpan duration, decimal pricePerHour) : base(duration, pricePerHour)
+        {
+           
+        }
+
+        public override decimal CalculateCost()
+        {
+            return 0;
+        }
+    }
+
+    public class PrivateVisit : Visit
+    {
+        public PrivateVisit(TimeSpan duration, decimal pricePerHour) : base(duration, pricePerHour)
+        {
+        }
+
+
+        public override decimal CalculateCost()
+        {
+            return (decimal)Duration.TotalHours * PricePerHour;
+        }
+    }
+
+    public class CompanyVisit : Visit
+    {
+        private const decimal companyDiscountPercentage = 0.9m;
+
+        public CompanyVisit(TimeSpan duration, decimal pricePerHour) : base(duration, pricePerHour)
+        {
+        }
+
+      
+
+        public override decimal CalculateCost()
+        {
+            return (decimal)Duration.TotalHours * PricePerHour * companyDiscountPercentage;
+        }
+    }
+
+    public abstract class Visit
     {
         public DateTime VisitDate { get; set; }
         public TimeSpan Duration { get; set; }
         public decimal PricePerHour { get; set; }
 
-        private const decimal companyDiscountPercentage = 0.9m;
+        public static string Symbol { get; private set; }
 
         public Visit(TimeSpan duration, decimal pricePerHour)
         {
@@ -17,24 +74,6 @@ namespace SimpleFactoryPattern
             PricePerHour = pricePerHour;
         }
 
-        public decimal CalculateCost(string kind)
-        {
-            decimal cost = 0;
-
-            if (kind == "N")
-            {
-                cost = 0;
-            }
-            else if (kind == "P")
-            {
-                cost = (decimal)Duration.TotalHours * PricePerHour;
-            }
-            else if (kind == "F")
-            {
-                cost = (decimal)Duration.TotalHours * PricePerHour * companyDiscountPercentage;
-            }
-
-            return cost;
-        }
+        public abstract decimal CalculateCost();       
     }
 }
