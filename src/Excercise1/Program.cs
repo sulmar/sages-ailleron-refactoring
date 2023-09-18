0a72c7cc25ee9b5ab558f608e209b73bf110b079
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-18 18:44:31 +0200

    Dodanie ćwiczenia

---
+++ src/Excercise1/Program.cs
@@ -0,0 +1,10 @@
+﻿using Excercise1;
+
+Ticket ticket = new Ticket { MovieName = "Akademia pana Kleksa", ReleaseYear = 1983, NumberOfSeats = 3 };
+
+ticket.Reserve(1, 'A');
+
+ticket.CalculatePrice(20);
+
+ticket.PrintTicket();
+ticket.SendByEmail("example@example.com");
---
+++ src/Excercise1/Ticket.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise1
+{
+    public class Ticket
+    {
+        public string MovieName { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public string Place { get; set; }
+
+        public decimal Price { get; set; }
+        public int NumberOfSeats { get; set; }
+
+
+        public void PrintTicket()
+        {
+            Console.WriteLine($"Movie: {MovieName} {ReleaseYear}, Place: {Place}");
+        }
+
+        public void SendByEmail(string emailAddress)
+        {
+            Console.WriteLine($"Ticket Movie: {MovieName} {ReleaseYear}, Place: {Place} sent to {emailAddress}");
+        }
+
+        public void GenerateRandomPlace()
+        {
+            Random random = new Random();
+            int row = random.Next(1, 11); // Random row between 1 and 10
+            char seat = (char)random.Next('A', 'J'); // Random seat between A and J
+
+            Place = $"{seat}{row}";
+        }
+
+        public void Reserve(int row, char seat)
+        {
+            Place = $"{seat}{row}";
+        }
+
+        public void CalculatePrice(decimal basePrice)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Monday)
+            {
+                Price = (basePrice * NumberOfSeats) * 0.8m; // Apply 20% discount for Monday
+            }
+            else
+            {
+                Price = basePrice * NumberOfSeats;
+            }
+        }
+    }
+
+}
