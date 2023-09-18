049b8c942853706b698e444dee27813fb1087afd
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-18 17:18:33 +0200

    Dodanie przykładu konwencji nazewniczej

---
+++ src/04_Refactoring/NamingConvention/Calculator.cs
@@ -0,0 +1,26 @@
+﻿namespace NamingConvention
+{
+    class Calculator
+    {
+        public decimal CalculateAndDisplayAmount(int quantity, decimal price)
+        {
+            var amount = Calculate(quantity, price);
+
+            DisplayAmount(amount);
+
+            return amount;
+        }
+
+        public decimal Calculate(int quantity, decimal price)
+        {
+            var amount = quantity * price;
+
+            return amount;
+        }
+
+        public static void DisplayAmount(decimal amount)
+        {
+            Console.WriteLine(amount);
+        }
+    }
+}
