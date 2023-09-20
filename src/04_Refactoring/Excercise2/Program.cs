706d19e6fc1817f1a661a771d530b15b989c7f57
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-20 08:29:45 +0200

    Dodanie ćwiczenia nr 2

---
+++ src/04_Refactoring/Excercise2/Program.cs
@@ -0,0 +1,58 @@
+﻿
+// TODO: Dodaj obliczanie kosztu wydruku do starej drukarki
+
+bool legacy = false;
+
+string document = "Hello World!";
+int copies = 3;
+
+
+if (legacy)
+{
+    LegacyPrinter printer = new LegacyPrinter();
+
+    for (int i = 0; i < copies; i++)
+    {
+        printer.PrintDocument(document);
+    }
+}
+else
+{
+    Printer printer = new Printer();
+    printer.Print(document, copies);
+}
+
+
+
+
+public class Printer
+{
+    decimal _costPerCopy = 0.1m; // Cost of 0.10 zł per copy
+
+    public void Print(string document, int copies = 1)
+    {
+        for (int copy = 1; copy <= copies; copy++)
+        {
+            Console.WriteLine($"Printer is printing: {document}");
+        }
+
+        var cost = CalculateCost(copies);
+        Console.WriteLine($"{cost:C2}");
+    }
+
+    private decimal CalculateCost(int copies)
+    {
+        // Calculate total cost based on the number of copies and cost per copy
+        return copies * _costPerCopy;
+    }
+}
+
+
+// Nie zmieniaj kodu tej starej klasy!
+public class LegacyPrinter
+{
+    public void PrintDocument(string document)
+    {
+        Console.WriteLine($"Legacy Printer is printing: {document}");
+    }
+}
