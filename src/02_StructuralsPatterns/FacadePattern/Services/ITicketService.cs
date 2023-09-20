288c38471ab7c77dcba1415aad4dddef7e4aef43
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-20 16:21:16 +0200

    Wydzielenie pliku

--- src/02_StructuralsPatterns/FacadePattern/Services/ITicketService.cs
+++ src/02_StructuralsPatterns/FacadePattern/Services/ITicketService.cs
@@ -1,5 +1,4 @@
 ﻿using FacadePattern.Models;
-using FacadePattern.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,48 +31,5 @@ namespace FacadePattern.Services
     {
         Ticket Buy(TicketParameters parameters); 
         void Cancel(Ticket ticket);
-    }
-
-    public class TicketService : ITicketService
-    {
-        private readonly IRailwayConnectionRepository railwayConnectionRepository;
-        private readonly TicketCalculator ticketCalculator;
-        private readonly ReservationService reservationService;
-        private readonly PaymentService paymentService;
-        private readonly EmailService emailService;
-
-        public TicketService(IRailwayConnectionRepository railwayConnectionRepository, 
-            TicketCalculator ticketCalculator, 
-            ReservationService reservationService, 
-            PaymentService paymentService, 
-            EmailService emailService)
-        {
-            this.railwayConnectionRepository = railwayConnectionRepository;
-            this.ticketCalculator = ticketCalculator;
-            this.reservationService = reservationService;
-            this.paymentService = paymentService;
-            this.emailService = emailService;
-        }
-
-        public Ticket Buy(TicketParameters parameters)
-        {
-            RailwayConnection railwayConnection = railwayConnectionRepository.Find(parameters.From, parameters.To, parameters.When);
-            decimal price = ticketCalculator.Calculate(railwayConnection, parameters.NumberOfPlaces);
-            Reservation reservation = reservationService.MakeReservation(railwayConnection, parameters.NumberOfPlaces);
-            Ticket ticket = new Ticket { RailwayConnection = reservation.RailwayConnection, NumberOfPlaces = reservation.NumberOfPlaces, Price = price };
-            Payment payment = paymentService.CreateActivePayment(ticket);
-
-            if (payment.IsPaid)
-            {
-                emailService.Send(ticket);
-            }
-
-            return ticket;
-        }
-
-        public void Cancel(Ticket ticket)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
