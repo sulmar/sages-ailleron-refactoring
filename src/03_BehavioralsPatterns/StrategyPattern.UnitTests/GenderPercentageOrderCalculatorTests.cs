a2826bdb5e53c7cf6acffc6513c0b0d367013baf
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-20 12:59:25 +0200

    Refaktoring

--- src/03_BehavioralsPatterns/StrategyPattern.UnitTests/GenderPercentageOrderCalculatorTests.cs
+++ src/03_BehavioralsPatterns/StrategyPattern.UnitTests/GenderPercentageOrderCalculatorTests.cs
@@ -5,47 +5,71 @@ using System;
 
 namespace StrategyPattern.UnitTests
 {
+    /*
+     * 
+    | From | To     | Gender | Percentage | Amount |   When   |
+      9:00   15:00     null     0.1          null
+      null   null     Female    0.2          null
+      null   null     Female    null          10
+
+    */
+
+
+    /* 
+      | Name Parameter | Value | 
+          Gender          Female
+        Percentage         0.1
+     * */
+
+    /*
+
+    | CanDiscountStrategy                                           | DiscountStrategy
+    <GenderCanDiscountStrategy>Female</GenderCanDiscountStrategy>   | <PercentageDiscountStrategy>0.1</PercentageDiscountStrategy>
+    <GenderCanDiscountStrategy>Male</GenderCanDiscountStrategy>   | <FixedDiscountStrategy>5</FixedDiscountStrategy>
+
+    */
+
     [TestClass]
-    public class GenderPercentageOrderCalculatorTests
-    {
-        private OrderCalculator calculator;
+public class GenderPercentageOrderCalculatorTests
+{
+  private OrderCalculator calculator;
 
-        [TestInitialize]
-        public void Init()
-        {
-            ICanDiscountStrategy canDiscountStrategy = new GenderCanDiscountStrategy(Gender.Female);
-            IDiscountStrategy discountStrategy = new PercentageDiscountStrategy(0.1m);
+  [TestInitialize]
+  public void Init()
+  {
+      ICanDiscountStrategy canDiscountStrategy = new GenderCanDiscountStrategy(Gender.Female);
+      IDiscountStrategy discountStrategy = new PercentageDiscountStrategy(0.1m);
 
-            calculator = new OrderCalculator(canDiscountStrategy, discountStrategy);
-        }
+      calculator = new OrderCalculator(canDiscountStrategy, discountStrategy);
+  }
 
-        [TestMethod]
-        public void CalculateDiscount_Female_ShouldDiscount()
-        {
-            // Arrange
-            Order order = new Order(DateTime.MinValue, new Customer { Gender = Gender.Female }, 100);
+  [TestMethod]
+  public void CalculateDiscount_Female_ShouldDiscount()
+  {
+      // Arrange
+      Order order = new Order(DateTime.MinValue, new Customer { Gender = Gender.Female }, 100);
 
-            // Act
-            decimal discount = calculator.CalculateDiscount(order);
+      // Act
+      decimal discount = calculator.CalculateDiscount(order);
 
-            // Assert
-            Assert.AreEqual(10, discount);
+      // Assert
+      Assert.AreEqual(10, discount);
 
-        }
+  }
 
-        [TestMethod]
-        public void CalculateDiscount_Male_ShouldNotDiscount()
-        {
-            // Arrange
-            Order order = new Order(DateTime.MinValue, new Customer { Gender = Gender.Male }, 100);
+  [TestMethod]
+  public void CalculateDiscount_Male_ShouldNotDiscount()
+  {
+      // Arrange
+      Order order = new Order(DateTime.MinValue, new Customer { Gender = Gender.Male }, 100);
 
-            // Act
-            decimal discount = calculator.CalculateDiscount(order);
+      // Act
+      decimal discount = calculator.CalculateDiscount(order);
 
-            // Assert
-            Assert.AreEqual(0, discount);
+      // Assert
+      Assert.AreEqual(0, discount);
 
-        }
+  }
 
-    }
+}
 }
--- src/03_BehavioralsPatterns/StrategyPattern/DiscountStrategies/PercentageDiscountStrategy.cs
+++ src/03_BehavioralsPatterns/StrategyPattern/DiscountStrategies/PercentageDiscountStrategy.cs
@@ -19,4 +19,41 @@
             return decimal.Zero;
         }
     }
+
+    // Lua
+    /*
+    public interface IScriptRuntimeAdapter
+    {
+        public decimal Execute(string script);
+    }
+
+    public class LuaScriptRuntimeAdapter : IScriptRuntimeAdapter
+    {
+        private Lua state = new Lua();
+        public decimal Execute(string script)
+        {
+            var res = state.DoString(script);
+
+            return res;
+        }
+    }
+
+    public class DynamicDiscountStrategy : IDiscountStrategy
+    {
+        string script = "order.Amount * percentage";
+
+        private ScriptRuntime scriptRuntime = new ScriptRuntime();
+
+        public decimal Discount(Order order)
+        {
+            // TODO: C# 
+            scriptRuntime.Execute()
+        }
+
+        public decimal NoDiscount()
+        {
+            // TODO: C#
+        }
+    }
+    */
 }
