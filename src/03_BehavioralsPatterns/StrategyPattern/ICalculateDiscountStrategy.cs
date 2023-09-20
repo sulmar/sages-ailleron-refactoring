f6c82ac6f9e8be195c82f981928cf78595446ac5
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-20 12:16:40 +0200

    Refaktoring - rozbicie na 2 interfejsy

--- src/03_BehavioralsPatterns/StrategyPattern/ICalculateDiscountStrategy.cs
+++ src/03_BehavioralsPatterns/StrategyPattern/ICalculateDiscountStrategy.cs
@@ -1,13 +1,26 @@
 ﻿namespace StrategyPattern
 {
     // Abstract Strategy
-    public interface ICalculateDiscountStrategy
+    public interface ICalculateDiscountStrategy : ICanDiscountStrategy, IDiscountStrategy
+    {
+        
+       
+    }
+
+    // Abstract Strategy A
+    public interface ICanDiscountStrategy
     {
         bool CanDiscount(Order order);
+    }
+
+    // Abstract Strategy B
+    public interface IDiscountStrategy
+    {
         decimal Discount(Order order);
         decimal NoDiscount();
     }
 
 
-    
+
+
 }
