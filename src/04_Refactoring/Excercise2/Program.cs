41b27755b3c661b9e7735b3f5100cd706b73a6be
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-20 10:47:53 +0200

    Refaktoring

--- src/04_Refactoring/Excercise2/Program.cs
+++ src/04_Refactoring/Excercise2/Program.cs
@@ -1,31 +1,40 @@
 ﻿
 // TODO: Dodaj obliczanie kosztu wydruku do starej drukarki
 
+using System;
+
 bool legacy = false;
 
 string document = "Hello World!";
 int copies = 3;
 
+IPrinterAdapter printer = new PrinterProxy(PrinterAdapterFactory.Create(legacy));
+printer.Print(document, copies);
 
-if (legacy)
+
+public class PrinterAdapterFactory
 {
-    LegacyPrinter printer = new LegacyPrinter();
-
-    for (int i = 0; i < copies; i++)
+    public static IPrinterAdapter Create(bool legacy)
     {
-        printer.PrintDocument(document);
+        if (legacy)
+        {
+            return new LegacyPrinterAdapter());            
+        }
+        else
+        {
+            return new Printer();            
+        }
     }
 }
-else
+
+// Abstract Adapter - powstał na podstawie klasy Printer, czyli nowej drukarki - chcemy dostosować starą drukarkę do nowej a nie odwrotnie :)
+public interface IPrinterAdapter
 {
-    Printer printer = new Printer();
-    printer.Print(document, copies);
+    void Print(string document, int copies = 1);
 }
 
 
-
-
-public class Printer
+public class Printer : IPrinterAdapter
 {
     decimal _costPerCopy = 0.1m; // Cost of 0.10 zł per copy
 
@@ -34,7 +43,38 @@ public class Printer
         for (int copy = 1; copy <= copies; copy++)
         {
             Console.WriteLine($"Printer is printing: {document}");
-        }
+        }        
+    }  
+}
+
+
+// Nie zmieniaj kodu tej starej klasy!
+public class LegacyPrinter
+{
+    public void PrintDocument(string document)
+    {
+        Console.WriteLine($"Legacy Printer is printing: {document}");
+    }
+}
+
+// Proxy
+public class PrinterProxy : IPrinterAdapter
+{
+    // Real Subject
+    private readonly IPrinterAdapter printer;
+
+    public PrinterProxy(IPrinterAdapter printer)
+    {
+        this.printer = printer;
+    }
+
+
+
+    decimal _costPerCopy = 0.1m; // Cost of 0.10 zł per copy
+
+    public void Print(string document, int copies = 1)
+    {
+        printer.Print(document, copies);
 
         var cost = CalculateCost(copies);
         Console.WriteLine($"{cost:C2}");
@@ -48,11 +88,21 @@ public class Printer
 }
 
 
-// Nie zmieniaj kodu tej starej klasy!
-public class LegacyPrinter
+// Concrete Adapter
+public class LegacyPrinterAdapter : IPrinterAdapter
 {
-    public void PrintDocument(string document)
+    private readonly LegacyPrinter printer;
+
+    public LegacyPrinterAdapter()
     {
-        Console.WriteLine($"Legacy Printer is printing: {document}");
+        printer = new LegacyPrinter();
+    }
+
+    public void Print(string document, int copies = 1)
+    {
+        for (int i = 0; i < copies; i++)
+        {
+            printer.PrintDocument(document);
+        }
     }
 }
